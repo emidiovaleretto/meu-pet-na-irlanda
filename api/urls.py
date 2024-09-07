38c0962ed8ef4b5b93540ecd06501ee165ee2e12
Emidio Valeretto <emidio.valereto@gmail.com>
2024-09-07 01:06:11 +0100

    Reorganize project's structure

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('v1/', views.photos_api_overview, name='photos_api_overview'),
-]
--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './App.css';
-import Api from './api/Api';
-
-const App = () => {
-  return <div className="text-3xl font-bold"><Api /></div>;
-};
-
-export default App;
