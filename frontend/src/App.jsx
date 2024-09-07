ec201ad60a141d15c6db3952cc3361cd7c44ad2e
Emidio Valeretto <emidio.valereto@gmail.com>
2024-09-07 01:05:47 +0100

    Reorganize frontend folder's structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Api from './api/Api';
+
+const App = () => {
+  return <div className="text-3xl font-bold"><Api /></div>;
+};
+
+export default App;
