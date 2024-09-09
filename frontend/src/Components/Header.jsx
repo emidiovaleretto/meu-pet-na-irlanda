14031585766c21de71c80a47d5cab3880864c0b1
Emidio Valeretto <emidio.valereto@gmail.com>
2024-09-09 18:18:58 +0000

    chore: style header

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from '../assets/logo.svg?react';
 
 const Header = () => {
   return (
-    <div>
-      <nav className='border-gray-200 bg-gray-900'>
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4 text-white">
-          <Link to='/'>Home</Link>
-          <Link to='/signin'>Sign in / Sign up</Link>
-        </div>
-      </nav>
-    </div>
+      <header className='header'>
+        <nav className="navbar">
+          <Link to='/'>
+            <Logo />
+          </Link>
+          <div className='space-x-2'>
+            <Link to='/signin'>Sign in</Link>
+            <Link to='/signup' className='signup'>Sign up</Link>
+          </div>
+        </nav>
+      </header>
   )
 }
 
