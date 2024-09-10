9e4316fd44feac3342e7d8c95ee2831fb77c7530
Emidio Valeretto <emidio.valereto@gmail.com>
2024-09-10 23:06:00 +0100

    chore: implement handleSubmit method

--- frontend/src/Components/Auth/SigninForm.jsx
+++ frontend/src/Components/Auth/SigninForm.jsx
@@ -2,18 +2,31 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const SigninForm = () => {
-  const [ username, setUsername ] = React.useState('');
-  const [ password, setPassword ] = React.useState('');
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-  function handleSubmit( event ) {
+  function handleSubmit(event) {
     event.preventDefault;
-    
+
+    fetch('', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    }).then((response) => {
+      console.log(response)
+      return response.json()
+    }).then((json) => {
+      console.log(json)
+    })
+
   }
 
   return (
     <section>
       <h1>Sign in</h1>
-      <form action="" onSubmit={ handleSubmit }>
+      <form action="" onSubmit={handleSubmit}>
         <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
         <input type="text" value={password} onChange={({ target }) => setPassword(target.value)} />
         <button>Sign in</button>
