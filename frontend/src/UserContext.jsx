ed8479eddc5e6e9afbb6fd716654203aa9c8b1af
Emidio Valeretto <emidio.valereto@gmail.com>
2024-09-12 09:56:01 +0100

    chore: create UserContext

---
+++ frontend/src/UserContext.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const UserContext = React.createContext()
+
+export const UserStorage = ({ children }) => {
+    const [data, setData] = React.useState(null)
+    const [login, setLogin] = React.useState(null)
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
+
+    async function getUser(token) {
+        const { url, options } = USER_GET(token)
+        const response = await fetch(url, options)
+        const json = await response.json()
+        setData(json)
+        setLogin(true)
+    }
+
+    async function userLogin(username, password) {
+        const { url, options } = TOKEN_POST({ username, password })
+        const response = await fetch(url, options)
+        const json = await response.json()
+        window.localStorage.setItem('token', json.token)
+        getUser(json.token)
+    }
+    return (
+        <UserContext.Provider value={{ userLogin }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
