139ba78bb94abd1c2ad80e203cf2c204a1a8157c
Emidio Valeretto <emidio.valereto@gmail.com>
2024-09-19 19:33:23 +0100

    chore: remove backend directory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer'
@@ -6,9 +7,31 @@ import Home from './Components/Home';
 import Auth from './Components/Auth/Auth';
 import './App.css';
 import { UserStorage } from './UserContext';
+import axios from 'axios';
 
 const App = () => {
+  const [posts, setPosts] = useState([]);
+  const endpoint = `${import.meta.env.VITE_API_URL}/posts/`;
+  async function fetchData() {
+    console.log('fetching data...');
+    const response = await axios(endpoint);
+    console.log(response);
+    const { data } = response;
+    setPosts(data);
+    console.log(data);
+    return data;
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return <div>
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.content}</li>
+      ))}
+    </ul>
     <BrowserRouter>
       <UserStorage>
         <Header />
