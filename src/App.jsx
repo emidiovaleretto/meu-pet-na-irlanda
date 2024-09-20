edf2c647bed114bb05edd8c7a2f64512132ec2ef
Emidio Valeretto <emidio.valereto@gmail.com>
2024-09-20 12:49:37 +0100

    chore: remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,7 @@ import { UserStorage } from './UserContext';
 import axios from 'axios';
 
 const App = () => {
-  const [posts, setPosts] = useState([]);
-  const endpoint = `${import.meta.env.VITE_API_URL}/posts/`;
-  async function fetchData() {
-    console.log('fetching data...');
-    const response = await axios(endpoint);
-    console.log(response);
-    const { data } = response;
-    setPosts(data);
-    console.log(data);
-    return data;
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return <div>
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id}>{post.content}</li>
-      ))}
-    </ul>
     <BrowserRouter>
       <UserStorage>
         <Header />
